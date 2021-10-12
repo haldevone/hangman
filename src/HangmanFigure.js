c734da6b2bd6947013cbef29427fcad5dbd3a062
haldevone <57771961+haldevone@users.noreply.github.com>
2021-10-12 19:46:54 +0200

    first init

---
+++ src/HangmanFigure.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HangmanFigure = () => {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default HangmanFigure;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+export const Header = () => {
+    return (
+        <div className="title-container">
+            <h1 className="main-title">Hangman</h1>
+        </div>
+    )
+}
+
+export default Header;
